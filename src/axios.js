2093375d4e7e57c8a614c364db741a187bd889e3
Rahul <63789293+Rahul-Das-Sarma@users.noreply.github.com>
2023-01-08 22:33:37 +0530

    update axiosjs

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const baseUrl= "https://damp-badlands-33929.herokuapp.com"  || "http://localhost:5000/"
+const baseUrl= "https://aflux-ecom-complete-rogq6u1k7-rahul-das-sarma.vercel.app"  || "http://localhost:5000/"
 
 const axiosInstances = axios.create({
     baseURL: baseUrl
